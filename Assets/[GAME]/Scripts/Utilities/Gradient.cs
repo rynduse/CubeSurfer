ca7ef733da07cdcac818e5a4fdebf9d308598938
rynduse <furkankodak40@gmail.com>
2021-01-25 20:43:16 +0300

    +add ConfettiParticle, Trail, GradientBackground

---
+++ Assets/[GAME]/Scripts/Utilities/Gradient.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gradient : MonoBehaviour
+{
+    public UnityEngine.UI.RawImage img;
+
+    private Texture2D backgroundTexture;
+
+    public void SetColor(Color color1, Color color2)
+    {
+        backgroundTexture = new Texture2D(1, 2);
+        backgroundTexture.wrapMode = TextureWrapMode.Clamp;
+        backgroundTexture.filterMode = FilterMode.Bilinear;
+        backgroundTexture.SetPixels(new Color[] { color1, color2 });
+        backgroundTexture.Apply();
+        img.texture = backgroundTexture;
+    }
+}
